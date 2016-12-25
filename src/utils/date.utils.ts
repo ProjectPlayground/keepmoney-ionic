681617f32205678f29f9757bc53848919f490784
volodymyro <volodymyro@interlink-ua.com>
2016-12-25 10:25:09 +0200

    add methods to check is date in current week

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,3 +1,7 @@
+import * as moment_ from 'moment';
+export const moment =  moment_["default"];
+import 'moment-range';
+
 export class DateUtils {
 
   constructor() {}
@@ -8,6 +12,18 @@ export class DateUtils {
 
   public static fromString(dateString: string): Date {
     return new Date(dateString);
+  }
+
+  public static isDayOfThisWeek(date: Date): boolean {
+    return moment.range(this.startOfWeek(), this.endOfWeek()).contains(date);
+  }
+
+  public static startOfWeek(): Date {
+    return moment(new Date()).startOf('week').isoWeekday(1).toDate();
+  }
+
+  public static endOfWeek(): Date {
+    return moment(new Date()).endOf('week').isoWeekday(0).toDate();
   }
 }
 
